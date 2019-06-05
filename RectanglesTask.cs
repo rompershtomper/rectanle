74c6eebef338d084dcebd33cc7e20330c52cd21a
rompershtomper <51242739+rompershtomper@users.noreply.github.com>
2019-06-05 19:57:23 +0300

    Update RectanglesTask.cs

--- RectanglesTask.cs
+++ RectanglesTask.cs
@@ -5,20 +5,20 @@
 		// Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
-            return (r1.Top <= r2.Bottom) && (r1.Left <= r2.Right) && (r1.Bottom >= r2.Top) && (r1.Right >= r2.Left); 
-        }
+			return (r1.Top <= r2.Bottom) && (r1.Left <= r2.Right) && (r1.Bottom >= r2.Top) && (r1.Right >= r2.Left); 
+		}
 
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
-            if (AreIntersected(r1, r2))
-            {
-                int left = System.Math.Max(r1.Left, r2.Left);
-                int right = System.Math.Min(r1.Right, r2.Right);
-                int top = System.Math.Max(r1.Top, r2.Top);
-                int bottom = System.Math.Min(r1.Bottom, r2.Bottom);
-                return (right - left) * (bottom - top);
-            }
+			if (AreIntersected(r1, r2))
+            		{
+				int left = System.Math.Max(r1.Left, r2.Left);
+				int right = System.Math.Min(r1.Right, r2.Right);
+				int top = System.Math.Max(r1.Top, r2.Top);
+				int bottom = System.Math.Min(r1.Bottom, r2.Bottom);
+				return (right - left) * (bottom - top);
+			}
 			return 0;
 		}
 
@@ -27,10 +27,11 @@
 		// Если прямоугольники совпадают, можно вернуть номер любого из них.
 		public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
 		{
-            if ((r1.Left <= r2.Left) && (r1.Right >= r2.Right) &&
-                (r1.Top <= r2.Top) && (r1.Bottom >= r2.Bottom)) return 1;
-            if ((r2.Left <= r1.Left) && (r2.Right >= r1.Right) && 
-                (r2.Top <= r1.Top) && (r2.Bottom >= r1.Bottom)) return 0;
+			if ((r1.Left <= r2.Left) && (r1.Right >= r2.Right) 
+			    && (r1.Top <= r2.Top) && (r1.Bottom >= r2.Bottom)) 
+				return 1;
+			if ((r2.Left <= r1.Left) && (r2.Right >= r1.Right) 
+			    && (r2.Top <= r1.Top) && (r2.Bottom >= r1.Bottom)) return 0;
 			return -1;
 		}
 	}
